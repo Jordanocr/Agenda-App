10fafc5979ff4bb4fde3bfc4f7fc616dbd14e8e7
Jordanocr <jordanocoelho5@gmail.com>
2025-11-03 16:09:27 -0300

    UI development.

--- src/ui.py
+++ src/ui.py
@@ -50,12 +50,12 @@ class MainMenu(ctk.CTk):
 
         self.createAppointmentButton = ctk.CTkButton(self,font=("Arial",20),text="Create appointment",
                                                      width=200,height=40,corner_radius=30,image=self.createAppointmentImage,
-                                                     border_width=1,command=lambda: self.openCreateAppointment(self))
+                                                     border_width=1,command=self.openCreateAppointment)
         self.createAppointmentButton.pack(pady=20)
 
         self.editAppointmentButton = ctk.CTkButton(self,font=("Arial",20),text="Edit appointments",
                                                    width=200,height=40,corner_radius=30,image=self.editAppointmentImage,
-                                                   border_width=1)
+                                                   border_width=1,command=self.openEditAppointment)
         self.editAppointmentButton.pack(pady=20)
 
         self.exportBackupButton = ctk.CTkButton(self,font=("Arial",20),text="Export backup",
@@ -68,11 +68,19 @@ class MainMenu(ctk.CTk):
                                          hover_color="#3b3b3b",command=self.openGithub)
         self.githubLabel.pack(pady=45)
 
-    def openCreateAppointment(self,parent):
-        parent.withdraw()
+    # ====== Methods & Commands ======
+    def openCreateAppointment(self):
+        """ Opens the create appointment interface and hides the main menu. """
+        self.withdraw()
         CreateAppointment(self)
 
+    def openEditAppointment(self):
+        """ Opens the edit appointment interface and hides the main menu. """
+        self.withdraw()
+        EditAppointment(self)
+
     def openGithub(self):
+        """ Opens the default browser and navigates to my GitHub profile. """
         webbrowser.open("https://github.com/Jordanocr")
 
 class CreateAppointment(ctk.CTkToplevel):
@@ -91,27 +99,65 @@ class CreateAppointment(ctk.CTkToplevel):
 
         Blank(self,py=10)
 
-        self.nameEntry = ctk.CTkEntry(self,font=("Arial",20),placeholder_text="Name",width=300,height=30,corner_radius=30)
-        self.nameEntry.pack(pady=20)
+        self.nameEntry = ctk.CTkEntry(self,font=("Arial",18),placeholder_text="Name:",width=300,height=30,corner_radius=30)
+        self.nameEntry.pack(pady=10)
 
-        self.descriptionLabel = ctk.CTkLabel(self,font=("Arial",20),text="Description")
+        self.descriptionLabel = ctk.CTkLabel(self,font=("Arial",18),text="Description:")
         self.descriptionLabel.pack(pady=5)
 
-        self.descriptionTextbox = ctk.CTkTextbox(self,font=("Arial",18),width=300,height=200,fg_color="#21244e",
+        self.descriptionTextbox = ctk.CTkTextbox(self,font=("Arial",16),width=400,height=200,fg_color="#21244e",
                                                  border_width=1,corner_radius=20)
         self.descriptionTextbox.pack()
 
-        self.dateEntry = ctk.CTkEntry(self,font=("Arial",20),placeholder_text="Date (dd/mm/yyyy)")
-        self.dateEntry.pack(pady=10)
+        self.dateEntry = ctk.CTkEntry(self,font=("Arial",18),placeholder_text="Date: DD/MM/YYYY",width=300,height=30,
+                                      corner_radius=30)
+        self.dateEntry.pack(pady=20)
+
+        self.reminderSwitch = ctk.CTkCheckBox(self,font=("Arial",14),text="Set a reminder",command=self.onCheck)
+        self.reminderSwitch.pack(pady=10)
 
         # ====== Protocols ======
         self.protocol("WM_DELETE_WINDOW",self.openMenu)
     
+    # ====== Methods & Commands ======
     def openMenu(self):
         self.parent.deiconify()
-        # Delays the deletion for safety reasons.
-        self.after(100,self.destroy)
+        self.destroy()
 
+    def onCheck(self):
+        """ Evaluates when the checkbox is activated if its value is 1 or 0. """
+        if self.reminderSwitch.get():
+            self.showReminderOption()
+        else:
+            self.hideReminderOption()
+
+    def showReminderOption(self):
+        """ Shows the reminder option. """
+        self.reminderTimeEntry = ctk.CTkEntry(self,font=("Arial",18),placeholder_text="Time: HH/MM",width=300,height=30,
+                                              corner_radius=30)
+        self.reminderTimeEntry.pack(pady=10)
+
+    def hideReminderOption(self):
+        """ Hides the reminder option. """
+        self.reminderTimeEntry.destroy()
+
+class EditAppointment(ctk.CTkToplevel):
+    """ Edit appointment interface. """
+    def __init__(self,parent):
+        super().__init__(parent)
+
+        # ====== Inicial settings ======
+        self.title("Edit appointment")
+        self.geometry("700x700")
+        self.parent = parent
+
+        # ====== Protocols ======
+        self.protocol("WM_DELETE_WINDOW",self.openMenu)
+    
+    # ====== Methods & Commands ======
+    def openMenu(self):
+        self.parent.deiconify()
+        self.destroy()
 
 if __name__ == "__main__":
     MainMenu().mainloop()
